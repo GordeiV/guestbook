a72088fd858ee70c2a751a72a3ab5555310f761b
Gordei Vikhliaev <ge42nuf@mytum.de>
2023-01-23 02:52:00 +0100

    .gitignore added

--- target/classes/static/app/application.js
+++
@@ -1,5 +0,0 @@
-(function(angular) {
-    angular.module("guestbook.controllers", []);
-    angular.module("guestbook.services", []);
-    angular.module("guestbook", ["ngResource", "guestbook.controllers", "guestbook.services"]);
-}(angular));
